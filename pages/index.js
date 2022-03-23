f5d6858b041d572a244d7accfa4ae96d7045af0b
Андрей <abobus21111@gmail.ru>
2022-03-23 16:52:52 +0300

    hotfix: js

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,21 @@ let popupElement = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.popup__close-button');
 let profileHeader = document.querySelector('.profile__header');
-let profileHeaderText = profileHeader.textContent;
+
 let profileText = document.querySelector('.profile__text');
-let profileTextAbout = profileText.textContent;
+
 let formElement = document.querySelector('.popup__form');
 let inputName = formElement.querySelector('.popup__input_type_name');
 let inputAbout = formElement.querySelector('.popup__input_type_about');
-let SubmitButton = formElement.querySelector('.popup__form-submit');
+
 
 
 function openForm (){
     popupElement.classList.add('popup_opened');
-    inputName = document.querySelector('.popup__input_type_name');
-    inputAbout = document.querySelector('.popup__input_type_about');
     inputName.value = profileHeader.textContent;
     inputAbout.value = profileText.textContent;
 }
-editButton.addEventListener('click', openForm, );
+editButton.addEventListener('click', openForm);
 
 
 function closeForm (){
