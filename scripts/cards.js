1c88626e0a558becc4d9b5a19233e23e63a7cf7a
Андрей <abobus21111@gmail.ru>
2022-04-18 16:09:25 +0300

    1

--- scripts/cards.js
+++ scripts/cards.js
@@ -23,4 +23,4 @@ const initialCards = [
 	  name: 'Бунарроти',
 	  link: 'https://images.unsplash.com/photo-1576016770956-debb63d92058?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80.jpg'
 	}
-  ];
+];
