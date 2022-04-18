1c1d59a64f1f3a52b9e00fcc2e72702ab6d07a62
Андрей <abobus21111@gmail.ru>
2022-04-18 16:38:43 +0300

    added: cards.js file

--- scripts/index.js
+++ scripts/index.js
@@ -27,32 +27,6 @@ const formObj = ({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error_active'
 }); 
-const initialCards = [
-	{
-	  name: 'Колизей',
-	  link: 'https://images.unsplash.com/photo-1647350737843-4e4f98139fc5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80.jpg'
-	},
-	{
-	  name: 'Витражи',
-	  link: 'https://images.unsplash.com/photo-1647793065821-8d8315cf20f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=708&q=80.jpg'
-	},
-	{
-	  name: 'Китайские праздники',
-	  link: 'https://images.unsplash.com/photo-1646729314120-be42ebffa28d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80.jpg'
-	},
-	{
-	  name: 'Запретный город',
-	  link: 'https://images.unsplash.com/photo-1547981609-4b6bfe67ca0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-	},
-	{
-	  name: 'Весна',
-	  link: 'https://images.unsplash.com/photo-1462275646964-a0e3386b89fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80.jpg'
-	},
-	{
-	  name: 'Бунарроти',
-	  link: 'https://images.unsplash.com/photo-1576016770956-debb63d92058?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80.jpg'
-	}
-];
 
 function closePopupWhenOverlayClick(evt) {
   if(evt.target.classList.contains('popup_opened')){
