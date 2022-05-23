c6263e4d2f516fbb5431f654ba17e019a4103dc2
Андрей <abobus21111@gmail.ru>
2022-05-23 20:31:03 +0300

    refactor: ways

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { PopupWithImage } from './PopupWithImage.js';
-import { Section } from './Section.js';
-import { PopupWithForm } from './PopupWithForm.js';
-import { UserInfo } from './UserInfo.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { Section } from '../components/Section.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
 
 const elementsPopup = document.querySelector('.popup_add');
 const buttonProfileEdit = document.querySelector('.profile__edit');
