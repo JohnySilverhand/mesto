0a0cfcb16a51c8304cd5d92c0dcba9a2947377f1
Андрей <abobus21111@gmail.ru>
2022-06-10 00:00:22 +0300

    sad

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import { UserInfo } from '../components/UserInfo.js';
 import { buttonProfileEdit, buttonAddProfile, editForm, inputName, inputAbout, buttonElementsSubmit, popupAddFormName, 
   popupAddFormImageLink, elementsContainer, userInfo, addForm, initialCards, formObj } from '../utils/contstants.js';
 import { Api } from '../components/Api';
+import { PopupDeleteImage } from '../components/PopupDeleteImage.js';
 
 const api = new Api({
 url: 'https://mesto.nomoreparties.co/v1/cohort-42',
@@ -18,14 +19,29 @@ headers: {
   'Content-Type':'application/json',
 }
 });
+let userId;
 
-const info = [api.getCards(), api.getProfileInfo()];
+Promise.all([
+  api.getCards(),
+  api.getProfileInfo()
+]).then(([card, user]) => {
+  userId = user._id;
+  createCard.renderItems(card);
+}).catch((err) => {
+  console.log(err);
+})
 
   const popupFormAdd = new PopupWithForm ({
   submitFormCallback: (cardData) => {
-    const popupNewCard = createNewCard(cardData);
-    createCard.addItem(popupNewCard);
-    popupFormAdd.close();
+    api.addCards(cardData)
+      .then((cardData) => {
+        const cardFromPopup = createNewCard(cardData);
+        createCard.addItem(cardFromPopup);
+        popupFormAdd.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 }, '.popup_add');
 popupFormAdd.setEventListeners();
@@ -51,12 +67,49 @@ const popupFormEdit =  new PopupWithForm ({
 }, '.popup_edit');
 popupFormEdit.setEventListeners();
 
+/*const popupFormDelete = new PopupDeleteImage ({
+  submitFormCallback: (card, element, id) => {
+    api.deleteCard(card, id)
+      .then((data) => {
+        element.remove();
+        popupFormDelete.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+}, '.popup-delete');
+popupFormDelete.setEventListeners();*/
+
 
 const createNewCard = (cardData) => {
   const card = new Card ({cardData, 
   handleCardClick: (name, link) => {
     openImagePopup.open(name, link);
-  }}, '#new-card');
+  },
+  deleteAddedCard: (card, id) => {
+    popupFormDelete.open(card, id);
+  },
+  likeNewCard: (card, id) => {
+    api.likeCard(card, id)
+    .then((cardData) => {
+      card.querySelector('.element__like').classList.add('element__like_active');
+      card.querySelector('.element__counter').textContent = cardData.likes.length; 
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },
+  dislikeCard: (card, id) => {
+    api.dislikeCard(card, id)
+    .then((cardData) => {
+      card.querySelector('.element__like').classList.remove('element__like_active');
+      card.querySelector('.element__counter').textContent = cardData.likes.length; 
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }}, userId, '#new-card');
   const cardsElement = card.createCard();
   return cardsElement;
 };
@@ -67,13 +120,11 @@ const addCardValidation = new FormValidator(formObj, addForm);
 addCardValidation.enableValidation();
 
 const createCard = new Section ({
-  items: initialCards,
-  renderer: (item) => {
-    const arrayCard = createNewCard(item);
-    createCard.addItem(arrayCard);
+  renderer: (cards) => {
+    createCard.addItem(createNewCard(cards));
   }
 }, '.elements');
-createCard.renderItems();
+
 
 buttonAddProfile.addEventListener('click', () => {
   addCardValidation.resetValidation();
